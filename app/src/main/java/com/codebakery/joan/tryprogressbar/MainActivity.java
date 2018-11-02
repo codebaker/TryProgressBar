7abe484b62f28271eb81f83a8129275a3a584bbd
codebaker <jungah.kim76@gmail.com>
2018-11-02 16:05:59 +0900

    깃수정

--- app/src/main/java/com/codebakery/joan/tryprogressbar/MainActivity.java
+++ app/src/main/java/com/codebakery/joan/tryprogressbar/MainActivity.java
@@ -20,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*
+        * 깃테스트
+        * 수정했음!!!!!!!!!!!!!!!!!!!!!!!
+        * */
         textView = (TextView) findViewById(R.id.text);
         tgButtton = (ToggleButton) findViewById(R.id.tglButton);
         button = (Button) findViewById(R.id.button);
