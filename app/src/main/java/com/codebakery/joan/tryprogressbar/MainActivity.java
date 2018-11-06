e2b350ff9240b05e4b136ffcdebcd1befa97d454
otter35 <otter35@naver.com>
2018-11-06 12:18:32 +0900

    Update MainActivity.java

--- app/src/main/java/com/codebakery/joan/tryprogressbar/MainActivity.java
+++ app/src/main/java/com/codebakery/joan/tryprogressbar/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     // First add commit
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // commant twe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
